e627d34d2cae4879c2aaa6c79b998a5c41f2d1e5
tedonema <marco.tedone@gmail.com>
2013-06-18 23:41:19 +0100

    Revert "PDM-3 Refined test case and added another wildcard example for a type"
    This reverts commit 5c97bdea4ca23b45dd3151b9ae564d5eb6f1f601.

--- src/test/java/uk/co/jemos/podam/test/unit/pdm3/Pdm3PojoUnitTest.java
+++ src/test/java/uk/co/jemos/podam/test/unit/pdm3/Pdm3PojoUnitTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
-import uk.co.jemos.podam.test.dto.pdm3.Pdm3APojo;
 import uk.co.jemos.podam.test.dto.pdm3.Pdm3Pojo;
 
 /**
@@ -24,23 +23,6 @@ public class Pdm3PojoUnitTest {
 		PodamFactory factory = new PodamFactoryImpl();
 		Pdm3Pojo pojo = factory.manufacturePojo(Pdm3Pojo.class);
 		Assert.assertNotNull(pojo);
-		Assert.assertNotNull("The collection element should not be null!",
-				pojo.getObjs());
-		Assert.assertFalse("The collection attribute should not be empty", pojo
-				.getObjs().isEmpty());
-
-	}
-
-	@Test
-	public void testPdm3APojo() {
-
-		PodamFactory factory = new PodamFactoryImpl();
-		Pdm3APojo pojo = factory.manufacturePojo(Pdm3APojo.class);
-		Assert.assertNotNull(pojo);
-		Assert.assertNotNull("The collection element should not be null!",
-				pojo.getObjs());
-		Assert.assertFalse("The collection attribute should not be empty", pojo
-				.getObjs().isEmpty());
 
 	}
 
