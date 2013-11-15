a89f2a9482aef00f5dd1de01dab3d3eadc75cd3d
Antony Stubbs <antony.stubbs@gmail.com>
2013-11-15 15:41:51 -0500

    Reduce excessive info level logging
    Protect debug statements from unnecessary string concatenation.

--- src/main/java/uk/co/jemos/podam/api/PodamFactoryImpl.java
+++ src/main/java/uk/co/jemos/podam/api/PodamFactoryImpl.java
@@ -216,7 +216,7 @@ public class PodamFactoryImpl implements PodamFactory {
 
 		final TypeVariable<?>[] typeParameters = pojoClass.getTypeParameters();
 		if (typeParameters.length > genericTypeArgs.length) {
-			LOG.info(pojoClass.getCanonicalName()
+			LOG.warn(pojoClass.getCanonicalName()
 					+ " is missing generic type arguments, expected "
 					+ typeParameters.length + " found "
 					+ genericTypeArgs.length + ". Returning null.");
@@ -416,13 +416,10 @@ public class PodamFactoryImpl implements PodamFactory {
 					Object[] constructorArgs = getParameterValuesForConstructor(
 							constructor, pojoClass, pojos, genericTypeArgs);
 
-					retValue = constructor.newInstance(constructorArgs);
+					LOG.debug("For class: {} a valid constructor: {} was found. PODAM will use it to create an instance.",
+							clazz.getName(), constructor);
 
-					LOG.info("For class: "
-							+ clazz.getName()
-							+ " a valid constructor: "
-							+ constructor
-							+ " was found. PODAM will use it to create an instance.");
+					retValue = constructor.newInstance(constructorArgs);
 
 					break;
 
@@ -1255,35 +1252,30 @@ public class PodamFactoryImpl implements PodamFactory {
 					retValue = (T) constructor.newInstance(parameterValues);
 					if (retValue instanceof Collection
 							&& ((Collection) retValue).size() == 0) {
-						LOG.info("We could create an instance with constructor: "
-								+ constructor
-								+ ", but collection is empty"
-								+ ". Will try with another one.");
+						LOG.debug("We could create an instance with constructor: {}, but collection is empty. Will try with another one.",
+								constructor);
 
 					} else if (retValue instanceof Map
 							&& ((Map) retValue).size() == 0) {
-						LOG.info("We could create an instance with constructor: "
-								+ constructor
-								+ ", but map is empty"
-								+ ". Will try with another one.");
-
+						LOG.debug("We could create an instance with constructor: {}, but map is empty. Will try with another one.",
+								constructor);
 					} else {
-						LOG.info("We could create an instance with constructor: "
-								+ constructor);
+						LOG.debug("We could create an instance with constructor: {}", constructor);
 						break;
 					}
 				} catch (Throwable t) {
-					LOG.warn("We couldn't create an instance for pojo: "
-							+ pojoClass + " for constructor: " + constructor
-							+ ". Will try with another one.");
+					if (LOG.isDebugEnabled()) {
+						LOG.debug("We couldn't create an instance for pojo: "
+								+ pojoClass + " for constructor: " + constructor
+								+ ". Will try with another one. " + t.getMessage());
+					}
 				}
 
 			}
 
 			if (retValue == null) {
-				LOG.warn("For class: " + pojoClass.getName()
-						+ " PODAM could not possibly create a value."
-						+ " This attribute will be returned as null.");
+				LOG.debug("For class: {}, PODAM could not possibly create a value. This attribute will be returned as null.",
+						pojoClass.getName());
 			}
 
 		}
@@ -1323,7 +1315,7 @@ public class PodamFactoryImpl implements PodamFactory {
 			final TypeVariable<?>[] typeParameters = pojoClass
 					.getTypeParameters();
 			if (typeParameters.length > genericTypeArgs.length) {
-				LOG.info(pojoClass.getCanonicalName()
+				LOG.warn(pojoClass.getCanonicalName()
 						+ " is missing generic type arguments, expected "
 						+ typeParameters.length + " found "
 						+ genericTypeArgs.length + ". Returning null.");
