9633368fdf2498b5f85101ef3d9e5bf21d7c9b56
Daniil Ivanov <daniil.ivanov@gmail.com>
2014-04-11 16:09:49 +0300

    Test for generic type parameters passed through inheritance implicitely

---
+++ src/test/java/uk/co/jemos/podam/test/dto/TypedClassPojo2.java
@@ -0,0 +1,13 @@
+package uk.co.jemos.podam.test.dto;
+
+import java.io.Serializable;
+
+/**
+ * Pojo derived from typed class
+ * 
+ * @author daivanov
+ *
+ */
+public class TypedClassPojo2 extends TypedClassPojo<String> {
+}
+
---
+++ src/test/java/uk/co/jemos/podam/test/unit/TypedClassTest.java
@@ -0,0 +1,23 @@
+package uk.co.jemos.podam.test.unit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+import uk.co.jemos.podam.test.dto.TypedClassPojo2;
+
+/**
+ * @author daivanov
+ *
+ */
+public class TypedClassTest {
+
+	@Test
+	public void testTypedClassInstantiation(){
+		PodamFactory factory = new PodamFactoryImpl();
+		TypedClassPojo2 pojo = factory.manufacturePojo(TypedClassPojo2.class);
+		Assert.assertNotNull(pojo);
+		Assert.assertNotNull(pojo.getTypedValue());
+		Assert.assertNotNull(pojo.getTypedList());
+	}
+}
