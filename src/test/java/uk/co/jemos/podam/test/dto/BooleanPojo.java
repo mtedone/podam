ab6af5ebda5fc95db55c7c71229d4743faade737
Daniil Ivanov <daniil.ivanov@gmail.com>
2014-12-18 02:30:22 +0200

    Test boolean fields filling and exclusion with annotations

---
+++ src/test/java/uk/co/jemos/podam/test/dto/BooleanPojo.java
@@ -0,0 +1,46 @@
+/**
+ *
+ */
+package uk.co.jemos.podam.test.dto;
+
+import uk.co.jemos.podam.test.dto.SimplePojoWithExcludeAnnotationToTestSetters.TestExclude;
+
+/**
+ * POJO to test when Podam sets a boolean.
+ *
+ * @author daivanov
+ *
+ */
+public class BooleanPojo {
+	private boolean value1;
+	private Boolean value2;
+	private Boolean value3;
+
+	public BooleanPojo() {
+	}
+
+	public boolean isValue1() {
+		return value1;
+	}
+
+	public void setValue1(boolean value1) {
+		this.value1 = value1;
+	}
+
+	@TestExclude
+	public Boolean isValue2() {
+		return value2;
+	}
+
+	public void setValue2(Boolean value2) {
+		this.value2 = value2;
+	}
+
+	public Boolean getValue3() {
+		return value3;
+	}
+
+	public void setValue3(Boolean value3) {
+		this.value3 = value3;
+	}
+}
---
+++ src/test/java/uk/co/jemos/podam/test/unit/BooleanUnitTest.java
@@ -0,0 +1,62 @@
+package uk.co.jemos.podam.test.unit;
+
+import java.io.Serializable;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+import uk.co.jemos.podam.api.RandomDataProviderStrategy;
+import uk.co.jemos.podam.common.PodamExclude;
+import uk.co.jemos.podam.test.dto.BooleanPojo;
+import uk.co.jemos.podam.test.dto.SimplePojoWithExcludeAnnotationToTestSetters.TestExclude;
+
+/**
+ * Test @uk.co.jemos.podam.test.dto.BooleanPojo@ construction
+ *
+ * @author daivanov
+ *
+ */
+public class BooleanUnitTest implements Serializable {
+	private static final long serialVersionUID = 8040488550006045568L;
+
+	private final static RandomDataProviderStrategy strategy =
+			RandomDataProviderStrategy.getInstance();
+
+	private final static PodamFactory podam = new PodamFactoryImpl(strategy);
+
+	@BeforeClass
+	public static void init() {
+		strategy.addExcludedAnnotation(TestExclude.class);
+		Assert.assertEquals("Unexpected number of exluded annotations",
+				2, strategy.getExcludedAnnotations().size());
+	}
+
+	@AfterClass
+	public static void deinit() {
+		strategy.removeExcludedAnnotation(TestExclude.class);
+		Assert.assertEquals("Unexpected number of exluded annotations",
+				1, strategy.getExcludedAnnotations().size());
+		Assert.assertTrue("Expected PodamExclude annotation, but found"
+				+ strategy.getExcludedAnnotations().toString(),
+				strategy.getExcludedAnnotations().contains(PodamExclude.class));
+	}
+
+	@Test
+	public void testBooleanFieldsSetting() throws Exception {
+	
+		BooleanPojo pojo = podam.manufacturePojo(BooleanPojo.class);
+		Assert.assertNotNull("Construction failed", pojo);
+
+		Assert.assertEquals("Should be filled", true, pojo.isValue1());
+
+		Assert.assertNull("Should be excluded", pojo.isValue2());
+
+		Assert.assertNotNull("Should be filled", pojo.getValue3());
+		Assert.assertEquals("Should be filled", true, pojo.getValue3());
+	}
+
+}
