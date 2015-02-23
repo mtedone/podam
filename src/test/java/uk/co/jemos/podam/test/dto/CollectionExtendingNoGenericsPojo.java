eb10b0b30bc14c6b4e1da25756b49c94b4c6690e
Daniil Ivanov <daniil.ivanov@gmail.com>
2015-02-23 21:15:19 +0200

    Test POJOs extending raw collections and maps

---
+++ src/test/java/uk/co/jemos/podam/test/dto/CollectionExtendingNoGenericsPojo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package uk.co.jemos.podam.test.dto;
+
+import java.util.LinkedList;
+
+/**
+ * Test pojo
+ * <p>
+ * Pojo extending Pojo with generic type.
+ * </p>
+ * 
+ * @author daivanov
+ * 
+ */
+@SuppressWarnings("rawtypes")
+public class CollectionExtendingNoGenericsPojo extends LinkedList {
+	private static final long serialVersionUID = 2004572368340270532L;
+}
---
+++ src/test/java/uk/co/jemos/podam/test/dto/MapExtendingNoGenericsPojo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package uk.co.jemos.podam.test.dto;
+
+import java.util.HashMap;
+
+/**
+ * Test pojo
+ * <p>
+ * Pojo extending Pojo with generic type.
+ * </p>
+ * 
+ * @author daivanov
+ * 
+ */
+@SuppressWarnings("rawtypes")
+public class MapExtendingNoGenericsPojo extends HashMap {
+	private static final long serialVersionUID = 5232955143368495291L;
+}
