8d60e6f8f714ae444f0aa53b06bbb657f3f9d795
Daniil Ivanov <daniil.ivanov@gmail.com>
2016-10-25 14:08:25 +0300

    Better class info validation

--- src/test/java/uk/co/jemos/podam/test/unit/steps/ClassInfoValidationSteps.java
+++ src/test/java/uk/co/jemos/podam/test/unit/steps/ClassInfoValidationSteps.java
@@ -1,10 +1,14 @@
 package uk.co.jemos.podam.test.unit.steps;
 
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import net.thucydides.core.annotations.Step;
+
 import org.junit.Assert;
+
 import uk.co.jemos.podam.api.ClassAttribute;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -16,18 +20,17 @@ public class ClassInfoValidationSteps {
     @Step("Then the attributes contains in ClassInfo should match the ones contains in the POJO")
     public void theClassInfoAttributesShouldMatchthePojoOnes(Set<String> attribs, Set<ClassAttribute> classAttributes) {
 
-        Assert.assertEquals("Wrong number of attributes" + classAttributes, attribs.size(),
-                classAttributes.size());
+        Set<String> missingAttribs = new HashSet<String>(attribs);
         for (ClassAttribute attribute : classAttributes) {
-            String attrName = attribute.getAttribute().getName();
-            if (!attribs.contains(attrName)) {
-                Assert.fail("Unexpected attribute " + attrName);
-            }
-            Assert.assertEquals("Wrong number of getters " + attribute.getGetters(),
+            String attrName = attribute.getName();
+            assertThat("Unexpected attribute", attrName, isIn(missingAttribs));
+            missingAttribs.remove(attrName);
+            Assert.assertEquals("Wrong number of getters for " + attribute.getName(),
                     1, attribute.getGetters().size());
-            Assert.assertEquals("Wrong number of setters " + attribute.getSetters(),
+            Assert.assertEquals("Wrong number of setters for " + attribute.getName(),
                     1, attribute.getSetters().size());
         }
+        assertThat("Missing attributes", missingAttribs, is(empty()));
     }
 
 }
