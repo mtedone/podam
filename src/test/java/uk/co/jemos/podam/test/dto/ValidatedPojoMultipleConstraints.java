1d8b300d161243274b0ff3a0dfac31b1de47fb88
Daniil Ivanov <daniil.ivanov@gmail.com>
2016-11-26 20:48:14 +0200

    Test negative value constraints for Issue #223

--- src/test/java/uk/co/jemos/podam/test/dto/ValidatedPojoMultipleConstraints.java
+++ src/test/java/uk/co/jemos/podam/test/dto/ValidatedPojoMultipleConstraints.java
@@ -17,12 +17,24 @@ public class ValidatedPojoMultipleConstraints {
     @Digits(integer = 1, fraction = 1)
     private Double value;
 
+    @DecimalMax("-0.1")
+    @Digits(integer = 1, fraction = 1)
+    private Double value2;
+
     public Double getValue() {
         return value;
     }
 
     public void setValue(Double value) {
         this.value = value;
+    }
+
+    public Double getValue2() {
+        return value2;
+    }
+
+    public void setValue2(Double value2) {
+        this.value2 = value2;
     }
 
     @Override
