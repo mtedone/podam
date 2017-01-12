0feb6dcdef58eb0f51e12b97c1912233ac78ba84
sba45 <Insider109>
2017-01-12 13:57:18 +0000

    Test multiple annotations attached to the same field

--- src/test/java/uk/co/jemos/podam/test/unit/features/annotations/AnnotationsTest.java
+++ src/test/java/uk/co/jemos/podam/test/unit/features/annotations/AnnotationsTest.java
@@ -9,6 +9,7 @@ import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.test.dto.ConstructorWithSelfReferencesPojoAndDefaultConstructor;
 import uk.co.jemos.podam.test.dto.ExcludeAnnotationPojo;
 import uk.co.jemos.podam.test.dto.ImmutableNoHierarchicalAnnotatedPojo;
+import uk.co.jemos.podam.test.dto.SimplePojoWithMultipleAnnotationsToAttribute;
 import uk.co.jemos.podam.test.dto.annotations.*;
 import uk.co.jemos.podam.test.strategies.ByteArrayStrategy;
 import uk.co.jemos.podam.test.unit.AbstractPodamSteps;
@@ -243,12 +244,12 @@ public class AnnotationsTest extends AbstractPodamSteps {
                 .getCharObjectFieldWithMinValueOnly();
         podamValidationSteps.theObjectShouldNotBeNull(charObjectFieldWithMinValueOnly);
         podamValidationSteps.theCharValueShouldBeGreaterOrEqualThan(charObjectFieldWithMinValueOnly,
-                (char)PodamTestConstants.NUMBER_INT_MIN_VALUE);
+                (char) PodamTestConstants.NUMBER_INT_MIN_VALUE);
 
         Character charObjectFieldWithMaxValueOnly = pojo
                 .getCharObjectFieldWithMaxValueOnly();
         podamValidationSteps.theCharValueShouldBeLowerOrEqualThan(charObjectFieldWithMaxValueOnly,
-                 PodamTestConstants.NUMBER_INT_ONE_HUNDRED);
+                PodamTestConstants.NUMBER_INT_ONE_HUNDRED);
 
         Character charObjectFieldWithMinAndMaxValue = pojo
                 .getCharObjectFieldWithMinAndMaxValue();
@@ -515,4 +516,15 @@ public class AnnotationsTest extends AbstractPodamSteps {
                 PodamTestConstants.POST_CODE, pojo.getPostCode());
     }
 
+
+    @Test
+    @Title("Podam should handle POJOs with multiple annotated fields, ignoring any non Podam annotation ")
+    public void podamShouldHandlePojosWithAnnotatedFieldsUsingHibernateConstraintsAnnotations() throws Exception {
+        PodamFactory podamFactory = podamFactorySteps.givenAStandardPodamFactory();
+        SimplePojoWithMultipleAnnotationsToAttribute pojo = podamInvocationSteps.whenIInvokeTheFactoryForClass(SimplePojoWithMultipleAnnotationsToAttribute.class, podamFactory);
+        podamValidationSteps.theObjectShouldNotBeNull(pojo);
+        podamValidationSteps.theIntegerObjectFieldShouldNotBeNull(pojo.getIntegerWithHibernateAnnotation());
+        podamValidationSteps.theTwoObjectsShouldBeEqual("stringFieldWithPatternRegex", pojo.getStringFieldWithPatternAnnotation());
+        podamValidationSteps.theTwoObjectsShouldBeEqual("testString", pojo.getStringFieldWithHibernateAnnotation());
+    }
 }
