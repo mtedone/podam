402b799e87833d29329b44f8e6e826667ffd40d1
Daniil Ivanov <daniil.ivanov@gmail.com>
2017-02-05 20:05:38 +0200

    Added randomness test for Issue #236

---
+++ src/test/java/uk/co/jemos/podam/test/unit/features/randomness/RandomnessTest.java
@@ -0,0 +1,39 @@
+package uk.co.jemos.podam.test.unit.features.randomness;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Title;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import uk.co.jemos.podam.api.PodamUtils;
+import uk.co.jemos.podam.test.unit.AbstractPodamSteps;
+
+/**
+ * Created by daivanov on 05/02/2017.
+ */
+@RunWith(SerenityRunner.class)
+public class RandomnessTest extends AbstractPodamSteps {
+
+	@Test
+	@Title("Podam should generate evenly distributed random integers")
+	public void podamShouldGenerateEvenlyDistributedRandomIntegers() throws Exception {
+		final int cnt = 10;
+		final int runs = 1000000;
+		int[] counters = new int[cnt];
+
+		for (int i = 0; i < runs; i++) {
+			int idx = PodamUtils.getIntegerInRange(0, cnt - 1);
+			counters[idx]++;
+		}
+
+		double mse = 0;
+		double expected = runs / cnt;
+		for (int i = 0; i < cnt; i++) {
+			double error = 100 * (counters[i] - expected) / expected;
+			mse += (error * error / cnt);
+		}
+		podamValidationSteps.theDoubleValueShouldBeBetween(mse, 0.0, 1.0);
+	}
+
+}
---
+++ src/test/java/uk/co/jemos/podam/test/unit/features/randomness/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains stories for randomness validation.
+ *
+ * Created by daivanov on 05/02/2017.
+ *
+ * @since 7.0.5
+ */
+package uk.co.jemos.podam.test.unit.features.randomness;
