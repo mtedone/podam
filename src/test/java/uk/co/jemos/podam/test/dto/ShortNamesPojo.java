7ad0ce84206333a51ef032535720f0f76de8184b
Daniil Ivanov <daniil.ivanov@gmail.com>
2022-06-26 21:14:48 +0300

    Test for Issue #304

---
+++ src/test/java/uk/co/jemos/podam/test/dto/ShortNamesPojo.java
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+package uk.co.jemos.podam.test.dto;
+
+/**
+ * POJO to test when Podam sets signle letter fields with number.
+ *
+ * @author daivanov
+ *
+ */
+public class ShortNamesPojo {
+	private String v1;
+
+	public String getV1() {
+		return v1;
+	}
+
+	public void setV1(String v1) {
+		this.v1 = v1;
+	}
+}
